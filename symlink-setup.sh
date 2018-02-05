3ec32571e4846f1fe4cb3fb53b82ac5f368b2f39
zsney <zsney.lzhou@gmail.com>
2018-02-05 09:23:26 +0800

    Add force remove old dotfiles

--- symlink-setup.sh
+++ symlink-setup.sh
@@ -1,3 +1,10 @@
+# Force remove the exists file
+rm -f ~/.zshrc
+rm -f ~/.vimrc
+rm -f ~/.gitignore
+rm -f ~/.gitconfig
+
+# Create Link
 ln ./.vimrc ~/.vimrc
 ln ./.zshrc ~/.zshrc
 ln ./.gitignore ~/.gitignore
