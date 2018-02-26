bf3639761e80e52af1064507116ba6be39c10958
zsney <zsney.lzhou@gmail.com>
2018-02-26 10:27:13 +0800

    Remove git-lfs, due to free edition limit to 1GB size

--- brew.sh
+++ brew.sh
@@ -7,7 +7,7 @@ brew upgrade
 # Install some useful tools
 brew install telnet
 brew install yarn # Instead of installed by npm
-brew install git-lfs # Install git large file storage
+# brew install git-lfs # Install git large file storage
 
 # Install z
 brew install z
