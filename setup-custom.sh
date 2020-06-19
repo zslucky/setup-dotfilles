12014d9bab0e73ec3f1188d25c90c593e140486a
luckyzhou <zsney.lzhou@gmail.com>
2020-06-19 17:19:37 +0800

    feat: Add git alias config

--- setup-custom.sh
+++ setup-custom.sh
@@ -1,6 +1,10 @@
 # setup git info
 # git config --global user.name "zsney"
 # git config --global user.email "zsney.lzhou@gmail.com"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
 
 # Update hostname
 # sudo scutil --set ComputerName "lzhou"
