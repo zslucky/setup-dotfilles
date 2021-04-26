9e61420e5aca5ddde92a6b24c2ae929d51ce3e13
luckyzhou <zsney.lzhou@gmail.com>
2021-04-26 15:33:22 +0800

    chore: update brew install cmd

--- setup-my-machine.sh
+++ setup-my-machine.sh
@@ -44,7 +44,7 @@ echo "------------------> xcode command line tool finished"
 ### Init install
 
 # Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install zsh
 brew install zsh
